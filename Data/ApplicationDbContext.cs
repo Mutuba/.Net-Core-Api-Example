933a1ef029f702521060afb2a4f7b45d55028a4d
Mutuba <danielmutubait@gmail.com>
2024-03-29 15:06:40 +0300

    Add an interface for repository

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
 using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace Data;
  
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityUserContext<IdentityUser>(options)
 {
+    // Entity class in code to be mapped to the database table
     public DbSet<Students> Students { get; set; }
 }
